5743fe5196851ff9c023270705ebca1cf206220c
Unknown <felipeaugusto.ferreiradecastro@gmail.com>
2017-05-20 22:27:15 -0300

    versao final do exercicio p4 da lista

--- ehprimo.c
+++ ehprimo.c
@@ -3,24 +3,28 @@
 #include<stdlib.h>
 #include<math.h>
 
-int main (){
-int n, r,primo = 0,teste;
-printf ("descubra se um numero inteiro eh primo :\n");
-printf("digite o numero desejado :");
-scanf("%d",&n);
- r = sqrt (n);
- do {
-        int d=0;
-  teste = n%d++;
-  if (teste){
-    printf ("o numero %d nao eh primo \n",n);
-    primo = 1;
+ int main (){
+  int n,d=2 ,raiz,p=1; //começarei supondo que o numero n é primo
+  printf ("descubra se o numero eh primo.\n");
+  do {//verificaçao do numero digitado.
+  printf ("digite um numero natural: ");
+  scanf("%d",&n);
+  }while(n<=0);
+  raiz=sqrt(n); //ja que um numero é primo se possuir divisores menores ou igual a sua raiz quadradada ,tiramos a raiz de n.
+
+while(d<=raiz ){
+
+    if(n%d++ == 0){
+        p = 0;
+        break;//se o ja foi encontrado um divisor de n ,nao é necessario proceguir.
+
   }
 
- }while ( r == d || primo == 1 );
- printf ("o numero %d  eh primo .\n",n);
+ }
 
-
+  if (p){
+        printf("o numero %d  eh primo.\n",n);}
+  else {printf ("o numero %d nao eh primo.\n",n);}
 return 0;
 }
 
