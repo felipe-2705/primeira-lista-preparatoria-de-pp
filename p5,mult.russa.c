473c6a232e5a368e871fab70298659cf8b566c7b
Unknown <felipeaugusto.ferreiradecastro@gmail.com>
2017-05-22 19:20:40 -0300

    exercicio p5 pronto

---
+++ p5,mult.russa.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+//faz uma multiplicaçao ao estilo russo , proposto no exercicio p5 da lista .
+int main()
+{
+    int n,n1,i,m,soma=0;//n= ao numero dividido,n1=o numero que será multiplicado e "soma" é o resultado da soma daqueles numeros q possuem seu correspondente impar.
+    printf("digite os numeros inteiros a serem multiplicados (sem o indicador de operador) :");
+    scanf("%d %d",&n,&n1);
+  i=n;
+  m=m1;
+    do{
+       if(n%2){
+        soma=soma+m;
+       }
+       i=i/2;
+       m=m*2;
+    }while(n>=1);  // ja que a divisao do termo da esquerda deve ocorrer ate um , colocamos como condiçao de repetiçao n ser maior ou igu
+    printf ("%d multiplicado por %d eh : %d",n,n1,soma);
+
+return 0 ;
+}
