226e72e80a77e20a2db9cb8b950e3a84e9fd7b96
Unknown <felipeaugusto.ferreiradecastro@gmail.com>
2017-05-23 17:23:01 -0300

    agr multiplica numeros negativos tb

--- p5,mult.russa.c
+++ p5,mult.russa.c
@@ -4,18 +4,24 @@
 int main()
 {
     int n,n1,i,m,soma=0;//n= ao numero dividido,n1=o numero que será multiplicado e "soma" é o resultado da soma daqueles numeros q possuem seu correspondente impar.
+    printf ("multiplicaçao russa de numeros inteiros.\n");
     printf("digite os numeros inteiros a serem multiplicados (sem o indicador de operador) :");
     scanf("%d %d",&n,&n1);
-  i=n;
-  m=m1;
-    do{
-       if(n%2){
-        soma=soma+m;
-       }
-       i=i/2;
-       m=m*2;
-    }while(n>=1);  // ja que a divisao do termo da esquerda deve ocorrer ate um , colocamos como condiçao de repetiçao n ser maior ou igu
-    printf ("%d multiplicado por %d eh : %d",n,n1,soma);
+    i= abs(n);// usarei o valor absoluto visto que é uma multiplicaçao e os sinais nao afetam o valor absoluto do produto.
+    m=abs(n1);
+    do
+    {
+        if(i%2)
+        {
+            soma=soma+m;
+        }
+        i=i/2;
+        m=m*2;
+    }
+    while(i>=1);   // ja que a divisao do termo da esquerda deve ocorrer ate um , colocamos como condiçao de repetiçao n ser maior ou igual a um.
+    if ((n>0&&n1>0)||(n<0&&n1<0))printf("o numero %d multiplicado por %d eh:%d",n,n1,soma);
+    if ((n<0&&n1>0)||(n>0&&n1<0))printf ("o numero %d multiplicado por %d eh :%d",n,n1,soma*-1);
+    if (n==0 || n1==0)printf("o numero %d multiplicado por %d eh:%d",n,n1,soma);
+    return 0 ;
 
-return 0 ;
 }
