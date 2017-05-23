26308921d094ed2ebf8dda958d765e6fdd89a398
Unknown <felipeaugusto.ferreiradecastro@gmail.com>
2017-05-23 17:49:16 -0300

    exercicio p6 completo

---
+++ conjectura de Collatz,exercicio p6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (){
+    int n;
+printf("conjectura de Collatz ou problema do (3x+1)\n");
+//verificar se o numero digitado eh natural
+do {
+    printf("digite um numero natural :");
+    scanf("%d",&n);
+}while(n<0);
+
+ do{
+        printf("%d\n",n);
+if (n%2){
+    n=(n*3)+1;
+}else {
+n=n/2;
+}
+
+
+ }while(n>1);
+ if(n==1)printf("1");//fazemos assim pois n pode ser zero .
+return 0 ;}
