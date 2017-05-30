5b398d1fef8a1ee059fbaa89a5f0f6db861b0c3c
BiancaCristina <phon-bia@hotmail.com>
2017-05-30 17:25:41 -0300

    Add files via upload
    arquivo final

--- salario.c
+++ salario.c
@@ -7,15 +7,15 @@ int main()
     char nome[30];
     int anos;
 
-    printf("Digite apenas o primeiro nome do engenheiro de software: ");
-    scanf("%s", nome);
+    printf("Digite nome do engenheiro de software: ");
+    scanf("%[^\n]s", nome); // %[^\n]s  usado pra conseguir armazenar espaços no nome
     printf("Digite a quantidade de anos trabalhada: ");
     scanf("%d", &anos);
 
     salarioinicial= 90000;
     salariohoje = 0.08*anos*salarioinicial + 0.01*anos*salarioinicial + salarioinicial;
 
-    if (salariohoje>150000) printf("Voce execedeu o teto salarial maximo de R$150000 para engenheiros de software.");
+    if (salariohoje>150000) printf("Voce execedeu o teto salarial maximo de R$150000 para engenheiros de software."); // salario limite
     else printf("%s trabalhou por %d anos. Salario atual: R$%.2f/anos", nome, anos, salariohoje);
 
     return 0;
